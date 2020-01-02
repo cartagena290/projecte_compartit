5a6044df029214e6f4a5548059af45892c5f6eb8
Xavi <xsuria@gmail.com>
2020-01-02 22:50:56 +0100

    commit test

--- index.php
+++ index.php
@@ -1,24 +1,13 @@
 <?php
 echo "Primer projecte compartit jenkins";
-
 echo "Modificio Laura";
 echo "Modificacio Xavi";
-
-
-
-
 echo "Modificacio Index";
-
 echo "Modificio Laura";
 echo "Modificacio Xavi";
 echo "Modificacio 2 Xavi";
-
 echo "hola caracola";
-
 echo "hola Laurita";
-
 echo "nova linia xavi";
-
 echo "rebase"
-
 ?>
